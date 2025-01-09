0550f3894a076b0c34f472efbb666073cebaccd1
Aaron Will Djaba <papiliocurtis@gmail.com>
2025-01-09 21:59:56 +0000

    updated login and logiut redirect urls

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,7 @@
 export const API_URL = "https://mi089ark7i.execute-api.eu-west-1.amazonaws.com/dev";
+export const LOGIN_REDIRECT= "https://task-manager-dev-ruby.vercel.app/dashboard";
+export const LOGOUT_REDIRECT= "https://task-manager-dev-ruby.vercel.app/logout";
+
+
+// export const LOGIN_REDIRECT= ""http://localhost:5173/dashboard";
+// export const LOGOUT_REDIRECT= ""http://localhost:5173/logout";
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {AuthProvider} from "react-oidc-context";
 import App from "@/App.tsx";
+import {LOGIN_REDIRECT} from "@/constants.ts";
 
 
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_QD3Q3j768",
     client_id: "7ed028j7isbb9je8k6sung8764",
-    redirect_uri: "http://localhost:5173/dashboard",
+    redirect_uri: LOGIN_REDIRECT,
     response_type: "code",
     scope: "email openid phone profile",
 };
